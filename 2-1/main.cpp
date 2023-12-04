43130079a189746eaa7abeb6defc96cdb95827af
Kody Striplin <kody.striplin@fawkesengineering.com>
2023-12-04 12:46:23 -0800

    WIP

--- 2-1/main.cpp
+++ 2-1/main.cpp
@@ -1,39 +1,63 @@
 #include <stdio.h>
+
 #include <fstream>
 #include <string>
 using namespace std;
 const int MAX_BLUE = 14;
 const int MAX_GREEN = 13;
 const int MAX_RED = 12;
-int main()
-{
-    int sum = 0;
-    ifstream f("input.txt");
-    if (f.is_open())
-    {
-        string line;
-        while (getline(f, line))
-        {
-            bool bLegalCount = true;
-            // get game ID
-            int gameId = line[5] - '0';
 
-            // split string by ;
-            string input = line.substr(line.find(":") + 2, line.size() - 1);
-            string outerDelimiter = ";";
-            size_t outerPos = 0;
-            string outerToken;
-            while ((outerPos = input.find(outerDelimiter)) != string::npos && bLegalCount) {
-                outerToken = input.substr(0, outerPos);
-                printf("outerToken: %s\n", outerToken.c_str());
-                input.erase(0, outerPos + outerDelimiter.length());
+bool isInt(char c) {
+    int x = (c - '0');
+    return (x >= 48 && x <= 57);
+}
 
-            }
-            // for each group, parse red, blue, green
-            // if each color less than or equal to max, add game id to sum
+bool legal(string game) {
+  string delimiter = ",";
+  string play;
+  size_t pos = 0;
+  bool bLegal = false;
+  while ((pos = game.find(delimiter)) != string::npos) {
+    play = game.substr(0, pos);
+
+
+    game.erase(0, pos + delimiter.length());
+  }
+  return true;
+}
+
+int main() {
+  int sum = 0;
+  ifstream f("input.txt");
+  if (f.is_open()) {
+    string line;
+    while (getline(f, line)) {
+      bool bLegalCount = true;
+      // get game ID
+      int gameIdStartPos = 5;
+      int colonPos = line.find(":");
+      string strGameId =
+          line.substr(gameIdStartPos, (colonPos - gameIdStartPos));
+      long iGameId = strtol(strGameId.c_str(), nullptr, 10);
+      string inputValues = line.substr((colonPos + 2));
+      string delimiter = ";";
+      string game;
+      size_t pos = 0;
+      bool bLegal = false;
+      while ((pos = inputValues.find(delimiter)) != string::npos) {
+        game = inputValues.substr(0, pos);
+        bLegal = legal(game);
+        if (!bLegal) {
+          break;
         }
+        inputValues.erase(0, pos + delimiter.length());
+      }
+      if (bLegal) {
+        sum += iGameId;
+      }
     }
-    printf("sum: %d\n", sum);
+  }
+  printf("sum: %d\n", sum);
 
-    return 0;
+  return 0;
 }
